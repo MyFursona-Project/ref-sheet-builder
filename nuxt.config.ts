00c1dcafaa1c1cd17d2e1575fcbe182a87786a58
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-11 00:37:56 +0800

    added buncha stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: [],
-	css: ['~/assets/css/main.scss'],
-	typescript: {
-		shim: false,
-		strict: true,
-	},
-	postcss: {
-		plugins: {
-			tailwindcss: {},
-			autoprefixer: {},
-		},
-	},
+  modules: [
+    "@nuxt/image-edge",
+    "@nuxtjs/color-mode",
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
+      },
+    ],
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "shortcut icon",
+          href: "./favicon.ico",
+        },
+        {
+          rel: "manifest",
+          href: "./manifest.json",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/css/main.scss"],
+  webpack: {
+    optimizeCSS: true,
+  },
+  typescript: {
+    strict: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
